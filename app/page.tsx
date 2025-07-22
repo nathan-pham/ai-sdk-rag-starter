1bc5870016cc6caec0d97e0a396cebcec3b1980a
nathan-pham <157486459+nathan-pham@users.noreply.github.com>
2025-07-22 10:18:04 +1000

    Update front end page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
+'use client';
+
+import { useChat } from '@ai-sdk/react';
+import { defaultChatStoreOptions } from 'ai';
+
 export default function Chat() {
-  return <main>Hello, world!</main>;
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    chatStore: defaultChatStoreOptions({
+      api: '/api/chat',
+    }),
+  });
+  return (
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      <div className="space-y-4">
+        {messages.map(m => (
+          <div key={m.id} className="whitespace-pre-wrap">
+            <div>
+              <div className="font-bold">{m.role}</div>
+              <p>{m.content}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
 }
